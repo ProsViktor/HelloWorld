8ffa53afd1e2d265a08532b2162d6f2a3c0b2621
Viktor Prosvetov <viktor.prosvetov@all.me>
2020-10-12 20:40:35 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Viktor Prosvetov on 12.10.2020.
+//  Copyright © 2020 Viktor Prosvetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
